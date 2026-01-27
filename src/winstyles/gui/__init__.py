073214b4f5738943f3ba9ae06c7e6c2766fa850b
chebbb <cheb2000@163.com>
2026-01-27 14:58:13 +0800

    fix: avoid gui module import warning

--- src/winstyles/gui/__init__.py
+++ src/winstyles/gui/__init__.py
@@ -2,6 +2,9 @@
 GUI 模块 - CustomTkinter 图形界面
 """
 
-from winstyles.gui.app import WinstyleSApp, WSSApp
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from winstyles.gui.app import WinstyleSApp, WSSApp
 
 __all__ = ["WinstyleSApp", "WSSApp"]
